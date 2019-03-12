a3aeeea810235ae94d119775670916a3ac7a1c74
SAMBA SIVA <sambasivanaidu6@gmail.com>
2019-03-12 13:54:57 +0530

    First Commit

---
+++ src/main/java/com/datarepository/CreationBean.java
@@ -0,0 +1,96 @@
+package com.datarepository;
+
+public class CreationBean {
+
+	private String enterFirstName;
+	private String enterLastName;
+	private String enterPassword;
+	private String enterCompany;
+	private String enterAddress;
+	private String enterCity;
+	private String selectState;
+	private String enterZipCode;
+	private String enterMobileNumber;
+	private String enterAlias;
+
+	public String getEnterFirstName() {
+		return enterFirstName;
+	}
+
+	public void setEnterFirstName(String enterFirstName) {
+		this.enterFirstName = enterFirstName;
+	}
+
+	public String getEnterLastName() {
+		return enterLastName;
+	}
+
+	public void setEnterLastName(String enterLastName) {
+		this.enterLastName = enterLastName;
+	}
+
+	public String getEnterPassword() {
+		return enterPassword;
+	}
+
+	public void setEnterPassword(String enterPassword) {
+		this.enterPassword = enterPassword;
+	}
+
+	public String getEnterCompany() {
+		return enterCompany;
+	}
+
+	public void setEnterCompany(String enterCompany) {
+		this.enterCompany = enterCompany;
+	}
+
+	public String getEnterAddress() {
+		return enterAddress;
+	}
+
+	public void setEnterAddress(String enterAddress) {
+		this.enterAddress = enterAddress;
+	}
+
+	public String getEnterCity() {
+		return enterCity;
+	}
+
+	public void setEnterCity(String enterCity) {
+		this.enterCity = enterCity;
+	}
+
+	public String getSelectState() {
+		return selectState;
+	}
+
+	public void setSelectState(String selectState) {
+		this.selectState = selectState;
+	}
+
+	public String getEnterZipCode() {
+		return enterZipCode;
+	}
+
+	public void setEnterZipCode(String enterZipCode) {
+		this.enterZipCode = enterZipCode;
+	}
+
+	public String getEnterMobileNumber() {
+		return enterMobileNumber;
+	}
+
+	public void setEnterMobileNumber(String enterMobileNumber) {
+		this.enterMobileNumber = enterMobileNumber;
+	}
+
+	public String getEnterAlias() {
+		return enterAlias;
+	}
+
+	public void setEnterAlias(String enterAlias) {
+		this.enterAlias = enterAlias;
+	}
+
+}
